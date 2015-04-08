c00c42a20bb10ef564a930c48f2f675d60c54ebb
mqzhang <zmingqian@qq.com>
2015-04-08 10:13:32 +0800

    change haosou domain

--- lib/serper/qihu360/crawler.rb
+++ lib/serper/qihu360/crawler.rb
@@ -2,14 +2,14 @@ class Serper::Qihu360
   def serp_url(keyword,page)
     keyword = keyword.gsub(" ","+")
     page = page.to_i > 1 ? "&pn=#{page.to_i}" : ''
-    URI.escape("http://www.so.com/s?q=#{keyword}#{page}&ie=utf-8&src=360sou_newhome")
+    URI.escape("http://www.haosou.com/s?q=#{keyword}#{page}&ie=utf-8&src=360sou_newhome")
   end
 
   def search(keyword,page)
     Serper::Crawler.get_serp(
         serp_url(keyword,page),
         {
-            'Referer' => 'http://www.so.com/',
+            'Referer' => 'http://www.haosou.com/',
             'User-Agent' => Serper::Crawler.rand_ua
         }).body
   end
